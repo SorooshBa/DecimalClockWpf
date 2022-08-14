c573befb44c9bfe486d117cab8d54ed76b49bb4a
SorooshBa <1231soroosh@gmail.com>
2022-08-14 16:35:27 +0430

    Add project files.

---
+++ DecClockWpf/DecimalTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecClockWpf
+{
+    internal class DecimalTime
+    {
+        private int dhour { get; set; }
+        private int dminut{ get; set; }
+        private double dsecond { get; set; }
+        public int Hours { get { Calculate(); return dhour; } }
+        public int Minuts { get { Calculate(); return dminut; } }
+        public double Seconds { get { Calculate(); return dsecond; } }
+
+        public override String ToString()
+        {
+            Calculate();
+            return dhour + ":" + dminut + ":" + (int)dsecond;
+        }
+        private void Calculate()
+        {
+            TimeSpan ts = DateTime.Now - new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            double tseconds = ts.TotalSeconds;
+            double seconds = (100000 * tseconds) / 86400;
+            dhour =(int) seconds / 10000;
+            dminut = (int)(seconds % 10000)/100;
+            dsecond = seconds % 100;
+        }
+
+    }
+}
---
+++ DecClockWpf/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DecClockWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0,0,0,0,1);
+            dispatcherTimer.Tick += new EventHandler(dispatcher_tick);
+            dispatcherTimer.Start();
+            dt = new DecimalTime();
+        }
+        DecimalTime dt;
+        private void dispatcher_tick(Object sender , EventArgs e)
+        {
+            setAngleForHands(dt.Hours, dt.Minuts, dt.Seconds);
+            
+        }
+        private void setAngleForHands(int hour,int min,double second)
+        {
+            double angleMin = (360 * min) / (double)100;
+            canMinHand.RenderTransform = new RotateTransform(angleMin);
+            double angleHour = (((hour * 100) + min) * (double)360) /(double) 1000;
+            canHouHand.RenderTransform = new RotateTransform(angleHour);
+            double angleSecond = (360 * second) / (double)100;
+            canSecHand.RenderTransform = new RotateTransform(angleSecond);
+        }
+
+
+    }
+}
